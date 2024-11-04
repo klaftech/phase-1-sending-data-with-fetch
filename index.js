7e1a780ac34d32ff990128f5308f330c5a9632dc
Elchonon <ekn400@gmail.com>
2024-11-04 18:43:29 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+const docBody = document.querySelector("body")
+
+function handleDomInsert(string){
+    const p = document.createElement("p")
+    p.textContent = string
+    docBody.append(p)
+}
+
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName,
+        email: userEmail,
+    }
+    const optionsObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    }
+    return fetch('http://localhost:3000/users',optionsObj)
+    .then((response) => response.json())
+    .then((data) => handleDomInsert(data.id))
+    .catch((error) => handleDomInsert(error))
+}
+
+function test(){
+    return submitData("Roger","roger@roger.com")
+}
